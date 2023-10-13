6768f2acb8007066bf942bdabccae97795abc036
pvelu1 <147877198+pvelu1@users.noreply.github.com>
2023-10-13 12:20:55 -0700

    CT1.java

--- CT1.java
+++ CT1.java
@@ -1,6 +1,6 @@
 public class CT1 {
     public static void main (String [] args) {
-      System.out.println("Olive Garden Italian Restaurant");          //ln prints output on new line
+      System.out.println("Olive Garden Italian Restaurant");
       System.out.println("1617 W Bethany Home Rd");       
       System.out.println("Phoenix, AZ 85015");
    }
