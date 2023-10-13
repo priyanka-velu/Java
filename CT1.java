594c2ea5eebe630166c17c16b00e35e762eafa08
pvelu1 <147877198+pvelu1@users.noreply.github.com>
2023-10-13 12:27:14 -0700

    Delete CT1.java

--- CT1.java
+++
@@ -1,7 +0,0 @@
-public class CT1 {
-    public static void main (String [] args) {
-      System.out.println("Olive Garden Italian Restaurant");
-      System.out.println("1617 W Bethany Home Rd");       
-      System.out.println("Phoenix, AZ 85015");
-   }
-}
